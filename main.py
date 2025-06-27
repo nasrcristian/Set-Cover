38720a45199bf11e60880901e2f4ad1e5566d901
nasrcristian <nasrcristian.un@gmail.com>
2025-06-27 12:35:29 -0300

    cambios en el algoritmo de dinamica

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from algoritmos.dinamica import dinamica_recursiva
 if __name__ == "__main__":
     print(dinamica_recursiva(
         {1, 2, 3, 4, 5},
-        [{1, 2}, {3, 4}, {4, 5}, {1, 3}, {2, 3, 4}, {1, 5}] 
+        [{1}, {2}, {1, 2}, {1, 2, 3}, {1, 3}, {1,2}, {1,4}, {4}, {5, 1}] 
     ))
